53a24a2d1245a551621dcaef9f6f32d088f720ef
mayank-2711 <minku2711@gmail.com>
2024-08-08 00:03:06 +0530

    Get impl

--- src/main/java/com/mayank/controller/EmployeeController.java
+++ src/main/java/com/mayank/controller/EmployeeController.java
@@ -1,7 +1,16 @@
 package com.mayank.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +25,25 @@ public class EmployeeController {
 	EmployeeRepository employeeRepository;
 	
 	@PostMapping("/employees")
-	public String createNewEmployee(Employee employee) {
+	public String createNewEmployee(@RequestBody Employee employee) {
 		employeeRepository.save(employee);
 		return "Employee created in DataBase";
 	}
-
+	
+	@GetMapping("/employees")
+	public ResponseEntity<List<Employee>> getAllEmployees() {
+		List<Employee> empList = new ArrayList<>();
+		employeeRepository.findAll().forEach(empList::add);
+		return new ResponseEntity<List<Employee>>(empList, HttpStatus.OK);
+	}
+	
+	@GetMapping("/employees/{emp_id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long emp_id) {
+		Optional<Employee> employee = employeeRepository.findById(emp_id);
+		if(employee.isPresent()) {
+			return new ResponseEntity<Employee>(employee.get(), HttpStatus.FOUND);
+		} else {
+			return new ResponseEntity<Employee>(HttpStatus.NOT_FOUND);
+		}
+	}
 }
--- src/main/java/com/mayank/model/Employee.java
+++ src/main/java/com/mayank/model/Employee.java
@@ -1,84 +1,83 @@
 package com.mayank.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "employee")
 public class Employee {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long empid;
-	
-	@Column(name = "emp_name")
-	private String emp_name;
-	
-	@Column(name = "emp_salary")
-	private Float emp_salary;
-	
-	@Column(name = "emp_age")
-	private Integer emp_age;
-	
-	@Column(name = "emp_city")
-	private String emp_city;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long empid;
+    
+    @Column(name = "emp_name")
+    private String emp_name;
+    
+    @Column(name = "emp_salary")
+    private Float emp_salary;
+    
+    @Column(name = "emp_age")
+    private Integer emp_age;
+    
+    @Column(name = "emp_city")
+    private String emp_city;
 
-	@Override
-	public String toString() {
-		return "Employee [empid=" + empid + ", emp_name=" + emp_name + ", emp_salary=" + emp_salary + ", emp_age="
-				+ emp_age + ", emp_city=" + emp_city + "]";
-	}
+    // No-argument constructor
+    public Employee() {}
 
-	public Employee(Long empid, String emp_name, Float emp_salary, Integer emp_age, String emp_city) {
-		super();
-		this.empid = empid;
-		this.emp_name = emp_name;
-		this.emp_salary = emp_salary;
-		this.emp_age = emp_age;
-		this.emp_city = emp_city;
-	}
+    // All-argument constructor
+    public Employee(Long empid, String emp_name, Float emp_salary, Integer emp_age, String emp_city) {
+        this.empid = empid;
+        this.emp_name = emp_name;
+        this.emp_salary = emp_salary;
+        this.emp_age = emp_age;
+        this.emp_city = emp_city;
+    }
 
-	public Long getEmpid() {
-		return empid;
-	}
+    // Getters and Setters
+    public Long getEmpid() {
+        return empid;
+    }
 
-	public void setEmpid(Long empid) {
-		this.empid = empid;
-	}
+    public void setEmpid(Long empid) {
+        this.empid = empid;
+    }
 
-	public String getEmp_name() {
-		return emp_name;
-	}
+    public String getEmp_name() {
+        return emp_name;
+    }
 
-	public void setEmp_name(String emp_name) {
-		this.emp_name = emp_name;
-	}
+    public void setEmp_name(String emp_name) {
+        this.emp_name = emp_name;
+    }
 
-	public Float getEmp_salary() {
-		return emp_salary;
-	}
+    public Float getEmp_salary() {
+        return emp_salary;
+    }
 
-	public void setEmp_salary(Float emp_salary) {
-		this.emp_salary = emp_salary;
-	}
+    public void setEmp_salary(Float emp_salary) {
+        this.emp_salary = emp_salary;
+    }
 
-	public int getEmp_age() {
-		return emp_age;
-	}
+    public Integer getEmp_age() {
+        return emp_age;
+    }
 
-	public void setEmp_age(Integer emp_age) {
-		this.emp_age = emp_age;
-	}
+    public void setEmp_age(Integer emp_age) {
+        this.emp_age = emp_age;
+    }
 
-	public String getEmp_city() {
-		return emp_city;
-	}
+    public String getEmp_city() {
+        return emp_city;
+    }
 
-	public void setEmp_city(String emp_city) {
-		this.emp_city = emp_city;
-	}
+    public void setEmp_city(String emp_city) {
+        this.emp_city = emp_city;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [empid=" + empid + ", emp_name=" + emp_name + ", emp_salary=" + emp_salary + ", emp_age="
+                + emp_age + ", emp_city=" + emp_city + "]";
+    }
 }
