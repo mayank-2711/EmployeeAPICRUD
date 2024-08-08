a1e1af18c23fa044ebb441c7f9f5bc7bab0dae03
mayank-2711 <minku2711@gmail.com>
2024-08-09 00:21:05 +0530

    more update and delete api

--- src/main/java/com/mayank/controller/EmployeeController.java
+++ src/main/java/com/mayank/controller/EmployeeController.java
@@ -7,9 +7,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,13 +39,37 @@ public class EmployeeController {
 		return new ResponseEntity<List<Employee>>(empList, HttpStatus.OK);
 	}
 	
-	@GetMapping("/employees/{emp_id}")
-	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long emp_id) {
-		Optional<Employee> employee = employeeRepository.findById(emp_id);
-		if(employee.isPresent()) {
-			return new ResponseEntity<Employee>(employee.get(), HttpStatus.FOUND);
+	@PutMapping("/employees/{emp_id}")
+	public String updateEmployeeById(@PathVariable Long emp_id, @RequestBody Employee employee) {
+		Optional<Employee> emp = employeeRepository.findById(emp_id);
+		if(emp.isPresent()) {
+			Employee existEmp = emp.get();
+			existEmp.setEmp_age(employee.getEmp_age());
+			existEmp.setEmp_city(employee.getEmp_city());
+			existEmp.setEmp_name(employee.getEmp_name());
+			existEmp.setEmp_salary(employee.getEmp_salary());
+			employeeRepository.save(existEmp);
+			return "Employee details for " + emp_id + " updated.";
 		} else {
-			return new ResponseEntity<Employee>(HttpStatus.NOT_FOUND);
-		}
+			return "Employee details doesn't exists";
+		} 
 	}
+	
+	@DeleteMapping("/employees/{emp_id}")
+	public String deleteEmployeeById(@PathVariable Long emp_id) {
+		Optional<Employee> emp = employeeRepository.findById(emp_id);
+		if(emp.isPresent()) {
+			employeeRepository.deleteById(emp_id);
+			return "Employee details for " + emp_id + " deleted.";
+		} else {
+			return "Employee details doesn't exists";
+		} 
+	}
+	
+	@DeleteMapping("/employees")
+	public String deleteEmployees() {
+		employeeRepository.deleteAll();
+		return "All Employees deleted Successfully..";
+	}
+		
 }
