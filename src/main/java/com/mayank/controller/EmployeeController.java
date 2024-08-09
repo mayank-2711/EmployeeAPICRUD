9218195a62ed981af60a2fd026a201895477fd0b
mayank-2711 <minku2711@gmail.com>
2024-08-10 00:00:06 +0530

    Age and city filters

--- src/main/java/com/mayank/controller/EmployeeController.java
+++ src/main/java/com/mayank/controller/EmployeeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mayank.model.Employee;
@@ -71,5 +72,17 @@ public class EmployeeController {
 		employeeRepository.deleteAll();
 		return "All Employees deleted Successfully..";
 	}
+	
+	@GetMapping("/employees/city")
+    public ResponseEntity<List<Employee>> getEmployeesByCity(@RequestParam String emp_city) {
+        List<Employee> employees = employeeRepository.findByEmp_city(emp_city);
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @GetMapping("/employees/age")
+    public ResponseEntity<List<Employee>> getEmployeesByAge(@RequestParam Integer emp_age) {
+        List<Employee> employees = employeeRepository.findByEmp_ageGreaterThanEqual(emp_age);
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
 		
 }
--- src/main/java/com/mayank/repository/EmployeeRepository.java
+++ src/main/java/com/mayank/repository/EmployeeRepository.java
@@ -1,9 +1,19 @@
 package com.mayank.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.mayank.model.Employee;
 
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+	
+	@Query(value = "SELECT * FROM employee WHERE emp_city=?1", nativeQuery = true)
+	List<Employee> findByEmp_city(String emp_city);
+	
+	@Query(value = "SELECT * FROM employee WHERE emp_age>=?1", nativeQuery = true)
+    List<Employee> findByEmp_ageGreaterThanEqual(Integer emp_age);
 
 }
